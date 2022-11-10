6b4e2d73b2667a4fc089df3973ccbafa2bf1d145
tiltaya <taialves95@gmail.com>
2022-11-10 20:56:55 -0300

    Exercícios Aula Condicionais I

---
+++ script.js
@@ -0,0 +1,35 @@
+// const bool1 = false
+// const bool2 = true
+// const bool3 = true
+
+// if(bool1 === bool2) {
+//     alert('O valor das variáveis 1 e 2 são iguais.')
+// } else if(bool2 === bool3) {
+//     alert('O valor das variáveis 2 e 3 são iguais.')
+// } else if(bool1 === bool3) {
+//     alert('O valor das variáveis 1 e 3 são iguais.')
+// } else {
+//     alert('O valor das variáveis não são iguais.')
+// }
+
+// const eleitor = 17
+
+// if(eleitor >= 18){
+//     console.log("Tem direito ao título de eleitor.")
+// } else if(eleitor >= 60 || eleitor >= 16 && eleitor < 18) {
+//     console.log("Voto facultativo.")
+// } else {
+//     console.log("Não tem direito ao título de eleitor.")
+// }
+
+const media = 7
+
+if(media >= 5) {
+    console.log("Aprovado.")
+} else if(media >= 3) {
+    console.log("Recuperação.")
+} else if(media - 3) {
+    console.log("Reprovado.")
+} else {
+    console.log("Dado inválido.")
+}
